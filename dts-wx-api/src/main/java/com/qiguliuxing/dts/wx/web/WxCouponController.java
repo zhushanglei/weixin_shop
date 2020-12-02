3ab6c71811fdbc58f867fabeae39aa4004ba513e
chenbo <623659388@qq.com>
2020-12-02 14:58:51 +0800

    店铺二维码海报及商铺支持管理员配置功能

--- dts-wx-api/src/main/java/com/qiguliuxing/dts/wx/web/WxCouponController.java
+++ dts-wx-api/src/main/java/com/qiguliuxing/dts/wx/web/WxCouponController.java
@@ -9,7 +9,6 @@ import java.util.Map;
 
 import javax.validation.constraints.NotNull;
 
-import com.github.pagehelper.PageInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -111,8 +110,7 @@ public class WxCouponController {
 
 		List<DtsCouponUser> couponUserList = couponUserService.queryList(userId, null, status, page, size, sort, order);
 		List<CouponVo> couponVoList = change(couponUserList);
-		long total = PageInfo.of(couponUserList).getTotal();
-		//int total = couponService.queryTotal();
+		int total = couponService.queryTotal();
 		Map<String, Object> data = new HashMap<String, Object>();
 		data.put("data", couponVoList);
 		data.put("count", total);
