1929fc9c7dab1ac1cfd70a60b663a60c76de8328
chenbo <623659388@qq.com>
2021-04-20 14:54:43 +0800

    按官方通知调整微信登录

--- wx-mini-program/pages/auth/login/login.js
+++ wx-mini-program/pages/auth/login/login.js
@@ -4,10 +4,17 @@ var user = require('../../../utils/user.js');
 
 var app = getApp();
 Page({
+  data: {
+    canIUseGetUserProfile: false, // 用于向前兼容
+  },
   onLoad: function(options) {
     // 页面初始化 options为页面跳转所带来的参数
     // 页面渲染完成
-
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
   },
   onReady: function() {
 
@@ -22,6 +29,30 @@ Page({
   onUnload: function() {
     // 页面关闭
 
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+    // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        user.checkLogin().catch(() => {
+          user.loginByWeixin(res.userInfo).then(res => {
+            app.globalData.hasLogin = true;
+            wx.navigateBack({
+              delta: 1
+            })
+          }).catch((err) => {
+            app.globalData.hasLogin = false;
+            util.showErrorToast('微信登录失败');
+          });
+        });
+      },
+      fail: (res) => {
+        app.globalData.hasLogin = false;
+        util.showErrorToast('微信登录失败');
+      }
+    });
   },
   wxLogin: function(e) {
     if (e.detail.userInfo == undefined) {
