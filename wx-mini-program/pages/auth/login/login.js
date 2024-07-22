7165a8cb5e0707df6f1fc93fb1535c4e2900ed11
zhushl <zhushanglei@yeah.net>
2024-07-22 14:59:30 +0800

    init

--- wx-mini-program/pages/auth/login/login.js
+++ wx-mini-program/pages/auth/login/login.js
@@ -1,7 +1,7 @@
 var api = require('../../../config/api.js');
 var util = require('../../../utils/util.js');
 var user = require('../../../utils/user.js');
-
+debugger
 var app = getApp();
 Page({
   data: {
@@ -31,6 +31,7 @@ Page({
 
   },
   getUserProfile(e) {
+    debugger
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
     // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
     wx.getUserProfile({
@@ -55,6 +56,7 @@ Page({
     });
   },
   wxLogin: function(e) {
+    debugger
     if (e.detail.userInfo == undefined) {
       app.globalData.hasLogin = false;
       util.showErrorToast('微信登录失败');
